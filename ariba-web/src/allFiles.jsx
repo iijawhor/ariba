bd255bb6ea5bf9c4427f2840b29c90f0a072a4a5
iijawhor <jawhorali1@gmail.com>
2025-08-31 11:50:51 +0530

    git addfiles to export

--- ariba-web/src/allFiles.jsx
+++ ariba-web/src/allFiles.jsx
@@ -1,2 +1,6 @@
-import Home from "./pages/Home";
-export { Home };
+import Dashboard from "./pages/Dashboard";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import LoginForm from "./components/LoginForm";
+export { Dashboard, Sidebar, Navbar, LoginPage, LoginForm };
