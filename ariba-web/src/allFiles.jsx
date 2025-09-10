53971bea4b29030ab40364da4e37bd6e90d812e6
iijawhor <jawhorali1@gmail.com>
2025-09-10 21:43:15 +0530

    files to export components and pages from one place

--- ariba-web/src/allFiles.jsx
+++ ariba-web/src/allFiles.jsx
@@ -1,6 +1,32 @@
-import Dashboard from "./pages/Dashboard";
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/Navbar";
-import LoginPage from "./pages/LoginPage";
-import LoginForm from "./components/LoginForm";
-export { Dashboard, Sidebar, Navbar, LoginPage, LoginForm };
+import Dashboard from "./pages/Dashboard.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import Navbar from "./components/Navbar.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import LoginForm from "./components/LoginForm.jsx";
+import StudentPage from "./pages/StudentPage.jsx";
+import StudentList from "./components/Student/StudentList.jsx";
+import StudentDetails from "./components/Student/StudentDetails.jsx";
+import MessageBox from "./components/MessageBox.jsx";
+import Anouncements from "./components/Anouncements.jsx";
+import TimelineModal from "./components/TimelineModal.jsx";
+import CreateUser from "./components/CreateUser.jsx";
+import TeacherDetails from "./components/Teacher/teacherDetails.jsx";
+import TeacherPage from "./pages/TeacherPage.jsx";
+import TeacherList from "./components/Teacher/TeacherList.jsx";
+export {
+  Dashboard,
+  Sidebar,
+  Navbar,
+  LoginPage,
+  LoginForm,
+  StudentPage,
+  StudentList,
+  StudentDetails,
+  Anouncements,
+  MessageBox,
+  TimelineModal,
+  CreateUser,
+  TeacherPage,
+  TeacherDetails,
+  TeacherList
+};
