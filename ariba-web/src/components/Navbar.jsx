a4bfd7053ba9f6525b879d12141c95b7046e43dd
iijawhor <jawhorali1@gmail.com>
2025-10-04 18:42:09 +0530

    fix(ui): adjust position of notification dot
    - Updated 	op and ight values to correctly align the notification indicator.

--- ariba-web/src/components/Navbar.jsx
+++ ariba-web/src/components/Navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = ({ activeMenu }) => {
   const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.loggedInUser?.user);
-
   const handleSearch = useCallback(() => {
     if (!searchQuery) {
       setError("Please enter something!");
@@ -33,7 +32,7 @@ const Navbar = ({ activeMenu }) => {
           </span>
           <span className="hidden md:inline h-4 w-[1px] bg-[#2C80FF]"></span>
           <span className="hidden md:inline text-gray-600 text-sm">
-            {user?.organization || "Kasbagola F.O.B Model High Madrasha"}
+            {user?.organization?.name || " Default organization ARIBA"}
           </span>
         </p>
       </div>
@@ -89,7 +88,7 @@ const Navbar = ({ activeMenu }) => {
               d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
             />
           </svg>
-          <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full bg-[#2C80FF] border-2 border-white"></span>
+          <span className="absolute top-1 right-2 block h-2.5 w-2.5 rounded-full bg-[#2C80FF] border-2 border-white"></span>
         </button>
 
         {/* User Dropdown */}
