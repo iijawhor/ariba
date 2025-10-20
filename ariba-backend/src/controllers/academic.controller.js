90cd97387ec871add5a7cc5f463a668c39085e93
iijawhor <jawhorali1@gmail.com>
2025-10-20 22:58:49 +0530

    Removed console

--- ariba-backend/src/controllers/academic.controller.js
+++ ariba-backend/src/controllers/academic.controller.js
@@ -6,7 +6,6 @@ export const createClass = async (req, res) => {
       .status(200)
       .json({ message: "Class created successfully", newClass });
   } catch (error) {
-    console.log("Failed to create Class ERROR......", error);
     return res.status(400).json({ message: "Failed tio create class" });
   }
 };
