c7a9d836002cd7060c3a91277a2d5e48d3262b7a
iijawhor <jawhorali1@gmail.com>
2025-10-22 22:53:35 +0530

    added academicReducer

--- ariba-web/src/store/store.js
+++ ariba-web/src/store/store.js
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./slices/searchSlice.js";
 import userReducer from "./slices/userSlice.js";
 import attendanceReducer from "./slices/atttendanceSlice.js";
-
+import academicReducer from "./slices/academicSlice.js";
 export const store = configureStore({
   reducer: {
     search: searchReducer,
     user: userReducer,
-    attendance: attendanceReducer
+    attendance: attendanceReducer,
+    academic: academicReducer
   }
 });
