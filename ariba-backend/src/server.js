dd296ead6eaa03f66796135b0911161f48386f66
iijawhor <jawhorali1@gmail.com>
2025-10-29 01:06:15 +0530

    Fix dotenv config for deployment

--- ariba-backend/src/server.js
+++ ariba-backend/src/server.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/db.js";
 import { app } from "./app.js";
-dotenv.config({ path: "./.env" });
+// dotenv.config({ path: "./.env" });
+dotenv.config();
 
 connectDB()
   .then(() => {
