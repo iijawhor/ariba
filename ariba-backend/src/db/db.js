c29820eb764d496b3673ed9e586295864059e5cb
iijawhor <jawhorali1@gmail.com>
2025-11-08 02:18:16 +0530

    revert: restore previous MongoDB connection setup

--- ariba-backend/src/db/db.js
+++ ariba-backend/src/db/db.js
@@ -1,34 +1,16 @@
 import { DB_NAME } from "../utils/constants.js";
-// import mongoose from "mongoose";
-// const connectDB = async () => {
-//   try {
-//     const connectionInstance = await mongoose.connect(
-//       `${process.env.MONGODB_URI}/${DB_NAME}`
-//     );
-//     console.log(
-//       `\n MongoDb connected !! DB HOST :${connectionInstance.connection.host}`
-//     );
-//   } catch (error) {
-//     console.log("MONGODB connection error ", error);
-//     process.exit(1);
-//   }
-// };
-// export { connectDB };
 import mongoose from "mongoose";
-
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
     console.log(
-      `MongoDB connected! DB HOST: ${connectionInstance.connection.host}`
+      `\n MongoDb connected !! DB HOST :${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.error("MONGODB connection error:", error.message);
+    console.log("MONGODB connection error ", error);
     process.exit(1);
   }
 };
-
 export { connectDB };
